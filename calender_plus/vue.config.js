9b34afa97503cba33ee755f0fc82b4a2e56e81b2
GADLO <geekarea@outlook.com>
2023-08-24 15:44:29 +0800

    🎉 init:proxy and lint change ,no use file delete

--- calender_plus/vue.config.js
+++ calender_plus/vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    overlay: {
+      wanrnings: false,
+      errors: false
+    },
+    proxy: {
+      "/api": {
+        target: "http://v.juhe.cn/",  //目标地址
+        changeOrigin: true,          //是否跨域，跨域改变源为true
+        ws: true,
+        secure: false,
+        pathRewrite: {
+          "^/api": ""
+        }
+      }
+    },
+    lintOnSave: false
+  },
 })
